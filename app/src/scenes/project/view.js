8281ec38746a20d98364b0de9c4115f5e2fb6952
vinesk <vinesk@users.noreply.github.com>
2024-11-26 12:38:13 +0100

    Update src/scenes/project/view.js to remove error due to undefined project's name

--- app/src/scenes/project/view.js
+++ app/src/scenes/project/view.js
@@ -70,7 +70,7 @@ const ProjectDetails = ({ project }) => {
               <div className="flex justify-between gap-2">
                 <div className="flex gap-20">
                   <span className="w-fit text-[20px] text-[#0C1024] font-bold">Nom du projet : </span>
-                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">{project.name.toString()}</span>
+                  <span className="w-fit text-[20px] text-[#0C1024] font-bold">{project?.name || ""}</span>
                 </div>
                 <div className="flex flex-1 flex-column items-end gap-3">
                   <Links project={project} />
